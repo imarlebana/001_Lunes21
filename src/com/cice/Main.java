d95dd57f3f4751c1ffc73f2f2cb1eba0487b7073
cice <cice@A8POVMac11.local>
2018-05-21 21:57:47 +0200

    Initial commit

---
+++ src/com/cice/Main.java
@@ -0,0 +1,21 @@
+package com.cice;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero entero:");
+        int num = sc.nextInt();
+
+        System.out.println("El cuadrado de " + num +" es: " + num*num );
+
+
+
+
+    }
+}
